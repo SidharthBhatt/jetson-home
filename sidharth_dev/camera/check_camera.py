61f26ba1be5d5449f16adb8db40a4ec17326cacc
Sidharth Bhatt <sidharthbhatt201907@gmail.com>
2026-06-29 06:40:02 +0800

    Auto-sync 2026-06-29 06:40:02

--- sidharth_dev/camera/check_camera.py
+++ sidharth_dev/camera/check_camera.py
@@ -3,4 +3,5 @@ do the same thing as you did in check_audio but for the camera.
 List all camera devices, check if /dev/video0 is present, and if it is, 
 try to open it and save the image, make sure the image published
 
+DO NOT CHANGE THIS COMMENT
 '''
