a2e41706e413dcd56f381d927b09fbcd6f2f079e
Sidharth Bhatt <sidharthbhatt201907@gmail.com>
2026-06-29 07:30:01 +0800

    Auto-sync 2026-06-29 07:30:01

---
+++ sidharth_dev/test_sensors/check_all_sensors.py
@@ -0,0 +1,7 @@
+'''
+List all the connected devices and their names which are easy to identify. E.g ORBBEC depth sensor is the microphone for audio input 
+Call all the specific check_* methods as we wrote in check_camera.py, check_audio.py, check_lidar.py, check_motors.py,
+Print the results in a table. 
+
+DO NOT TOUCH
+'''
