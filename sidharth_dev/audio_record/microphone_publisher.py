1df7f283ae2c10f0c7b383275921b541cda21018
Sidharth Bhatt <sidharthbhatt201907@gmail.com>
2026-07-01 06:11:12 +0800

    Auto-sync 2026-07-01 06:11:12

--- sidharth_dev/audio_record/microphone_publisher.py
+++ sidharth_dev/audio_record/microphone_publisher.py
@@ -55,7 +55,9 @@ class MicrophonePublisher(Node):
         #     self.get_logger().warn('no audio')
         #     return
         audio = whisper.pad_or_trim(audio)
-        self.raw_pub.publish(audio)
+        msg_audio = AudioData()
+        msg_audio.data = audio.tolist()
+        self.raw_pub.publish(msg_audio)
        
 
         # make log-Mel spectrogram and move to the same device as the model
@@ -84,6 +86,7 @@ class MicrophonePublisher(Node):
         self.txt_pub.publish(msg_txt)
     def artificial_callback(self):
         # mic is broken so we will use this for now 
+        subprocess.run([ffmpeg -v quiet -i "<output2026-06-25 06:13:10.663178.mp3>" -f s16le -ac 1 -ar 16000 pipe:1], capture_output=True)
         msg_txt = String()
         msg_txt.data = "This is an artificial callback message [insert audio here]"
         self.txt_pub.publish(msg_txt)
