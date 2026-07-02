fb68be9981977bde462c2d494ba0780531472a8a
Sidharth Bhatt <sidharthbhatt@Sidharths-MacBook-Air-2.local>
2026-07-01 23:12:36 -0500

    insert styuff here

--- sidharth_dev/lidar/lidar_publisher.py
+++ sidharth_dev/lidar/lidar_publisher.py
@@ -5,44 +5,88 @@ from rclpy.node import Node
 '''
 Termial 1: run robot bringup
 
-source /opt/ros/humble/setup.bash
-source ~/yahboomcar_ros2_ws/yahboomcar_ws/install/setup.bash
-ros2 launch ydlidar_ros2_driver ydlidar_launch.py
+1. at Terminal 1, turn on raidar
+    source /opt/ros/humble/setup.bash
+    source ~/yahboomcar_ros2_ws/yahboomcar_ws/install/setup.bash
+    ros2 launch ydlidar_ros2_driver ydlidar_launch.py
 
 
+2. at terminal 2, turn on robot control access
+    source /opt/ros/humble/setup.bash
+    source ~/yahboomcar_ros2_ws/yahboomcar_ws/install/setup.bash
+    ros2 launch yahboomcar_bringup yahboomcar_bringup_R2_launch.py
+
+
+3. at terminal 3, run code
+    cd ~/sidharth_dev/lidar_publisher
+    python3 lidar_publisher.py
 
 '''
 
-class MinimalPublisher(Node):
+class LidarPublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        super().__init__('lidar_publisher')
+        scan_qos = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=10
+        )
+        self.scan_subscriber = self.create_subscription(LaserScan, '/scan', self.scan_callback, scan_qos)
+        self.cloud_publisher_ = self.create_publisher(PointCloud2, '/scan_points', 10)
+
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+       points = []
+
+        angle = scan_msg.angle_min
+
+        for distance in scan_msg.ranges:
+            if not math.isinf(distance) and not math.isnan(distance):
+                if scan_msg.range_min < distance < scan_msg.range_max:
+                    x = distance * math.cos(angle)
+                    y = distance * math.sin(angle)
+                    z = 0.0
+
+                    points.append([x, y, z])
+
+            angle += scan_msg.angle_increment
+
+        fields = [
+            PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
+            PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
+            PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
+        ]
+
+        cloud_msg = point_cloud2.create_cloud(
+            scan_msg.header,
+            fields,
+            points
+        )
+
+        self.cloud_publisher.publish(cloud_msg)
+
+        self.get_logger().info(f'Published point cloud with {len(points)} points')
 
 
 def main(args=None):
-    rclpy.init(args=args)
+    rclpy.init()
 
-    minimal_publisher = MinimalPublisher()
+    lidar_publisher = LidarPublisher()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(lidar_publisher)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
-    rclpy.shutdown()
+     try:
+        rclpy.spin(lidar_publisher)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        lidar_publisher.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
-    main(
+    main()
